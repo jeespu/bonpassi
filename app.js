b59941d270a4dce4b3327b1c48e290488b9e225f
DESKTOP-8IGEAB3\jesse <jessekamarainen@hotmail.com>
2019-04-14 15:38:28 +0300

    ADD mysql connection shit

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+var mysql = require('mysql');
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err;
+
+  console.log('The solution is: ', rows[0].solution);
+});
+
+connection.end();
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
