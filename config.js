4c0a85c8c70feb7e938e3f7110f462acbd46d1d0
DESKTOP-8IGEAB3\jesse <jessekamarainen@hotmail.com>
2019-04-14 19:27:14 +0300

    vihdoin jotain toimivaa

---
+++ config.js
@@ -0,0 +1,15 @@
+var config = {
+	database: {
+		host:	  'q7cxv1zwcdlw7699.chr7pe7iynqr.eu-west-1.rds.amazonaws.com', 	// database host
+		user: 	  'rkhs71ydpn11g37c', 		// your database username
+		password: 'zauyntbmzwxhi29l', 		// your database password
+		port: 	  3306, 		// default MySQL port
+		db: 	  'fg62ry4ik09ydqu1' 		// your database name
+	},
+	server: {
+		host: '127.0.0.1',
+		port: '3000'
+	}
+}
+
+module.exports = config
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,17 @@
-module.exports = {
-    getHomePage: (req, res) => {
-        let query = "SELECT * FROM `players` ORDER BY id ASC"; // query database to get all the players
+var express = require('express')
+var app = express()
 
-        // execute query
-        db.query(query, (err, result) => {
-            if (err) {
-                res.redirect('/');
-            }
-            res.render('index.ejs', {
-                title: "Welcome to Socka | View Players"
-                ,players: result
-            });
-        });
-    },
-};
+app.get('/', function(req, res) {
+	// render to views/index.ejs template file
+	res.render('index', {title: 'My Node.js Application'})
+})
+
+/** 
+ * We assign app object to module.exports
+ * 
+ * module.exports exposes the app object as a module
+ * 
+ * module.exports should be used to return the object 
+ * when this file is required in another module like app.js
+ */ 
+module.exports = app;
