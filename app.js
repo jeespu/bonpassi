907d41470f13e2475d74d59174552252db695ee0
DESKTOP-8IGEAB3\jesse <jessekamarainen@hotmail.com>
2019-04-14 18:18:00 +0300

    uusiks koko paska

--- app.js
+++ app.js
@@ -1,72 +1,51 @@
-// Testikommentti
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const path = require('path');
+const app = express();
 
-var mysql = require('mysql');
-var JAWSDB_URL = 'mysql://rkhs71ydpn11g37c:zauyntbmzwxhi29l@q7cxv1zwcdlw7699.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/fg62ry4ik09ydqu1'
-try {
-    var connection = mysql.createConnection(JAWSDB_URL);
-    console.log("Connection succesful!")
-}
-catch (e) {
-    console.log("Connection unsuccesful")
-}
+const {getHomePage} = require('./routes/index');
+const {addPlayerPage, addPlayer, deletePlayer, editPlayer, editPlayerPage} = require('./routes/player');
+const port = 3000;
 
-connection.connect();
- 
-connection.query('SELECT * FROM USER', function (error, results, fields) {
-  if (error) throw error;
-  console.log('The solution is: ', results);
-});
- 
-connection.end();
-
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// create connection to database
+// the mysql.createConnection function takes in a configuration object which contains host, user, password and the database name.
+const db = mysql.createConnection ({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'socka'
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// connect to database
+db.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('Connected to database');
 });
+global.db = db;
 
-const port = process.env.PORT || 3000
+// configure middleware
+app.set('port', process.env.port || port); // set express to use this port
+app.set('views', __dirname + '/views'); // set express to look in this folder to render our view
+app.set('view engine', 'ejs'); // configure template engine
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json()); // parse form data client
+app.use(express.static(path.join(__dirname, 'public'))); // configure express to use public folder
+app.use(fileUpload()); // configure fileupload
+
+// routes for the app
+
+app.get('/', getHomePage);
+app.get('/add', addPlayerPage);
+app.get('/edit/:id', editPlayerPage);
+app.get('/delete/:id', deletePlayer);
+app.post('/add', addPlayer);
+app.post('/edit/:id', editPlayer);
+
+// set the app to listen on the port
 app.listen(port, () => {
-console.log('Server is up on port ' + port)
-})
-
-module.exports = {
-    app,
-    connection
-}
-
-
+    console.log(`Server running on port: ${port}`);
+});
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+module.exports = {
+    getHomePage: (req, res) => {
+        let query = "SELECT * FROM `players` ORDER BY id ASC"; // query database to get all the players
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Jyväskylä-appi' });
-});
-
-module.exports = router;
+        // execute query
+        db.query(query, (err, result) => {
+            if (err) {
+                res.redirect('/');
+            }
+            res.render('index.ejs', {
+                title: "Welcome to Socka | View Players"
+                ,players: result
+            });
+        });
+    },
+};
