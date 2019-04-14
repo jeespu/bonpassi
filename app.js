20c112f8b8411805a5ce8347a13369e81bec89ed
DESKTOP-8IGEAB3\jesse <jessekamarainen@hotmail.com>
2019-04-14 15:48:06 +0300

    asd

--- app.js
+++ app.js
@@ -4,16 +4,6 @@ if (connection) {
     console.log("Connection succesful!")
 };
 
-connection.connect();
-
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-  if (err) throw err;
-
-  console.log('The solution is: ', rows[0].solution);
-});
-
-connection.end();
-
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
