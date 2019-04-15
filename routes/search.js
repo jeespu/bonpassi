a206f49bbccc68a833cf110bf8eff53a07dfb051
DESKTOP-8IGEAB3\jesse <jessekamarainen@hotmail.com>
2019-04-16 01:15:26 +0300

    searchia vähä

--- routes/search.js
+++ routes/search.js
@@ -6,7 +6,7 @@ app.get('/', function (req, res, next) {
    // render to views/user/register.ejs
    res.render('searchresults', {
        isLoggedIn: req.session.isLoggedIn,
-      title: 'Search results for ' + req.query.keyword,
+       title: 'Hakutulokset kohteelle ' + req.query.keyword,
        keyword: req.query.keyword
    })
 });
