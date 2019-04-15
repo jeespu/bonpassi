5caf5e8a98c13ccec342617851239e82b023e50e
DESKTOP-8IGEAB3\jesse <jessekamarainen@hotmail.com>
2019-04-16 01:11:03 +0300

    searchia vähä

--- routes/search.js
+++ routes/search.js
@@ -2,10 +2,12 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res, next) {
+    var keyword = req.body.keyword;
    // render to views/user/register.ejs
    res.render('searchresults', {
        isLoggedIn: req.session.isLoggedIn,
-      title: 'Search results'
+      title: 'Search results for ' + req.query.keyword,
+       keyword: req.query.keyword
    })
 });
 
